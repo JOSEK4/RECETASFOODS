e920a91acda3e4e0e40c057ed0aee90965bf60ce
jose ortiz <jose.a.121k4@gmail.com>
2025-01-21 23:07:36 -0500

    Creando el menu

--- src/app/registro/registro.module.ts
+++ src/app/registro/registro.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';  // Importa los módulos necesarios
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 import { IonicModule } from '@ionic/angular';
-import { RegistroPageRoutingModule } from './registro-routing.module';  // Asegúrate de que el routing esté configurado
+import { RegistroPageRoutingModule } from './registro-routing.module';  
 import { RegistroPage } from './registro.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,  // Importante para formularios reactivos
+    ReactiveFormsModule,  
     IonicModule,
     RegistroPageRoutingModule
   ],
