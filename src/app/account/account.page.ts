5ed8fb5ef1e71a0b113d7d61b649e1420ed1a9b3
Jose <jortiz6969@misena.edu.co>
2025-01-29 10:07:54 -0500

    feat: agregar funcionalidad de edición de perfil; permitir modificar nombre, apellido y usuario, y actualizar foto de perfil

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,10 +1,11 @@
-
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Storage } from '@ionic/storage-angular';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 defineCustomElements(window);
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
@@ -13,7 +14,10 @@ defineCustomElements(window);
 })
 export class AccountPage implements OnInit {
   user_data: any;
+  editedUser: any = {};
   loadingUserData: boolean = false;
+  editMode: boolean = false;
+
   constructor(
     private userService: UserService,
     private storage: Storage
@@ -21,50 +25,70 @@ export class AccountPage implements OnInit {
     this.getUser();
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-  async getUser(){
+  async getUser() {
     this.loadingUserData = true;
     let user: any = await this.storage.get('user');
     console.log(user);
+
     this.userService.getUser(user.id).then(
       (data: any) => {
-
         console.log(data);
         this.storage.set('user', data);
         this.user_data = data;
+        this.editedUser = { ...data };
         this.loadingUserData = false;
       }
-    ).catch(
-      (error) => {
-        console.log(error);
-        this.loadingUserData = false;
-      });
+    ).catch((error) => {
+      console.log(error);
+      this.loadingUserData = false;
+    });
+  }
+
+  toggleEditMode() {
+    if (this.editMode) {
+      this.saveChanges();
+    } else {
+      this.editedUser = { ...this.user_data };
+    }
+    this.editMode = !this.editMode;
+  }
+
+  async saveChanges() {
+    this.userService.updateUser(this.editedUser).then(
+      (data) => {
+        console.log('Usuario actualizado:', data)
+        this.user_data = { ...this.editedUser }; 
+        this.storage.set('user', this.user_data);
+      }
+    ).catch((error) => {
+      console.log('Error al actualizar usuario:', error);
+    });
   }
 
   async takePhoto() {
-    console.log('Take Photo');
+    console.log('Tomando foto...');
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.DataUrl,
       source: CameraSource.Camera,
       quality: 100
     });
-    console.log(capturedPhoto.dataUrl);
-    this.user_data.image = capturedPhoto.dataUrl;
-    this.update();
+
+    console.log('Foto capturada:', capturedPhoto.dataUrl);
+    this.editedUser.image = capturedPhoto.dataUrl;
+    this.updatePhoto();
   }
 
-  async update() {
-    this.userService.updateUser(this.user_data).then(
+  async updatePhoto() {
+    this.userService.updateUser({ image: this.editedUser.image }).then(
       (data) => {
-        console.log(data);
+        console.log('Foto actualizada:', data);
+        this.user_data.image = this.editedUser.image;
+        this.storage.set('user', this.user_data);
       }
-    ).catch(
-      (error) => {
-        console.log(error);
-      });
+    ).catch((error) => {
+      console.log('Error al actualizar foto:', error);
+    });
   }
-
 }
