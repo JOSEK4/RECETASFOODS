bdaf466e1b9753117a0d24a4836c2ae258c959e0
Jose <jortiz6969@misena.edu.co>
2025-01-29 14:40:37 -0500

    feat: agregar botón para redirigir a la página de inicio de sesión en el registro; mejorar estilos y animaciones

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -10,25 +11,22 @@ import { NavController } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
   registerForm: FormGroup;
-  errorMessage: any;
+  errorMessage: string = '';
+
   formErrors = {
     user: [
       { type: 'required', message: 'El usuario es obligatorio' },
-      { type: 'minlength', message: 'El usuario debe tener al menos 5 caracteres' },
-
+      { type: 'minlength', message: 'El usuario debe tener al menos 5 caracteres' }
     ],
     name: [
-      { type: 'required', message: 'El nombre es obligatorio' },
-
+      { type: 'required', message: 'El nombre es obligatorio' }
     ],
     lastname: [
-      { type: 'required', message: 'El apellido es obligatorio' },
-
+      { type: 'required', message: 'El apellido es obligatorio' }
     ],
-
     email: [
       { type: 'required', message: 'El correo es obligatorio' },
-      { type: 'email', message: 'El correo no es valido' }
+      { type: 'email', message: 'El correo no es válido' }
     ],
     password: [
       { type: 'required', message: 'La contraseña es obligatoria' },
@@ -36,42 +34,40 @@ export class RegisterPage implements OnInit {
     ],
     passwordConfirmation: [
       { type: 'required', message: 'La confirmación de la contraseña es obligatoria' },
-      {
-        type: 'minlength',
-        message: 'La confirmación de la contraseña debe tener al menos 6 caracteres',
-      },
+      { type: 'minlength', message: 'La confirmación de la contraseña debe tener al menos 6 caracteres' }
     ],
-  }
+  };
+
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private navCtrl: NavController
   ) {
     this.registerForm = this.formBuilder.group({
-      user: ['', [Validators.required]],
+      user: ['', [Validators.required, Validators.minLength(5)]],
       name: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      passwordConfirmation: [
-        '',
-        [Validators.required, Validators.minLength(6)],
-      ],
+      passwordConfirmation: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  registerUser(registerData: any) {
+    this.authService.register(registerData)
+      .then(() => {
+        this.errorMessage = '';
+        this.navCtrl.navigateForward('/login');
+      })
+      .catch((err: any) => {
+        console.log(err);
+        this.errorMessage = err;
+      });
   }
 
-  registerUser(registerData: any){
-    this.authService.register(registerData).then((res: any) => {
-      console.log(res);
-      this.errorMessage = '';
-      this.navCtrl.navigateForward('/login');
-    }).catch((err: any) => {
-      console.log(err);
-      this.errorMessage = err;
-    });
+  goToLogin() {
+    this.navCtrl.navigateBack('/login');
   }
-
 }
